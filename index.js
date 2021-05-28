ad76a54ba6228a06c306fe871a3db1c83a22c07e
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2021-05-28 03:39:34 -0700

    Completed adding bot functions
    Created and completed making all of the bot functions.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
  * Bot Initialization:
  */
 
-// Discord client initialization:
+//Discord client initialization:
 require('dotenv').config();
 var Discord=require('discord.js');
 const client=new Discord.Client();
+const fs=require('fs');
+const readline=require('readline');
 client.login(process.env.CLIENT_TOKEN);
 
 //Variables:
@@ -15,3 +17,55 @@ let WYR_FILE=process.env.WYR_FILE;
 
 var topics=[];
 var wyr=[];
+
+
+/**
+ * Bot Functions:
+ */
+
+//Reads a file from a given file.
+function readFile(file){
+    const rl=readline.createInterface({
+        input=fs.createReadStream(file),
+        output: process.stdout,
+        terminal: false
+    });
+    if(file==TOPICS_FILE){
+        rl.on('line', (line) => {
+            topics.push(line);
+        });
+    }
+    else if(file==WYR_FILE){
+        rl.on('line', (line) => {
+            wyr.push(line);
+        });
+    }
+    else{
+        console.log("Error: Read File - Incorrect file provided.");
+    }
+}
+
+//Appends a prompt to a file.
+function addLine(file, line){
+    fs.appendFile(file, '\n'+line, (err) => {
+        if(err){
+            console.log("Error: Adding line \'"+line+"\' to "+file+".");
+        }
+        else{
+            console.log("Successfully added prompt to "+file+".");
+        }
+    });
+}
+
+//Gets a random prompt from a given prompt type.
+function getPrompt(type){
+    if(type=='topics'){
+        return topics[Math.floor(Math.random()*topics.length)];
+    }
+    else if(type=='wyr'){
+        return wyr[Math.floor(Math.random()*wyr.length)];
+    }
+    else{
+        console.log("Error: Get Prompt - Incorrect type provided.");
+    }
+}
